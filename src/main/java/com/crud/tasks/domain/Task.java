669ce5cfcb301df6136dfdb07f6a2398b4c61f11
Marcin Kolbe <marcin.kolbe@gmail.com>
2025-02-06 15:18:25 +0100

    18.2 Zadanie: wyszukiwanie pojedynczego zadania

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -1,12 +1,23 @@
 package com.crud.tasks.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tasks")
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String title;
+
+    @Column(name = "description")
     private String content;
 }
